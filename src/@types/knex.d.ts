c7ca4cf6583484391369a74c76e0a92b47257a15
Matheus Viscki <matheusviscki@gmail.com>
2024-06-06 07:22:27 -0300

    feat: Create types for Fastify tables.

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,26 @@
+import 'knex'
+
+interface IUser {
+  id: string
+  name: string
+  email: string
+  password: string
+  created_at: Date
+}
+
+interface IFood {
+  id: string
+  name: string
+  description: string
+  in_diet: boolean
+  created_at: Date
+  updated_at?: Date
+  user_id: string
+}
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: IUser
+    foods: IFood
+  }
+}

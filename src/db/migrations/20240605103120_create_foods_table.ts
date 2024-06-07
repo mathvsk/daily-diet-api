640cffc727f140c73da1ea0b14f98487dc3fe119
Matheus Viscki <matheusviscki@gmail.com>
2024-06-07 19:25:13 -0300

    refactor: Remove optional from the "in_diet" field

--- src/db/migrations/20240605103120_create_foods_table.ts
+++ src/db/migrations/20240605103120_create_foods_table.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex): Promise<void> {
     table.uuid('id').primary()
     table.string('name').notNullable()
     table.string('description').notNullable()
-    table.boolean('in_diet').defaultTo(false)
+    table.boolean('in_diet').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
     table.timestamp('updated_at').nullable()
 
